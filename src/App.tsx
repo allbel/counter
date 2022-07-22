ef52a92ec1bafa5db46562d5f83a4522ab68a801
Aleksandr Belozerov <allbel900@gmail.com>
2022-07-22 16:14:42 +0300

    Hook useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,33 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const [value, setValue] = useState<number>(0)
+    const [value, setValue] = useState<number>(0)
 
-  const incHandler = () => {
-    setValue(value + 1)
-  }
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }, [])
 
-  const setToLocalStorageHandler = () => {
-      localStorage.setItem('counterValue', JSON.stringify(value))
-      localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
-  }
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }, [value])
 
-  const getFromLocalStorageHandler = () => {
-      let valueAsString = localStorage.getItem('counterValue')
-      if (valueAsString) {
-          let newValue = JSON.parse(valueAsString)
-          setValue(newValue)
-      }
-  }
+    const incHandler = () => {
+        setValue(value + 1)
+    }
 
-  const clearLocalStorage = () => {
-      localStorage.clear()
-      setValue(0)
-  }
-
-  const removeItemFromLocalStorage = () => {
-      localStorage.removeItem('counterValue + 1')
-  }
-
-  return (
-    <div className="App">
-      <h1>{value}</h1>
-      <button onClick={incHandler}>inc</button>
-      <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
-      <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
-      <button onClick={clearLocalStorage}>clearLocalStorage</button>
-      <button onClick={removeItemFromLocalStorage}>removeItemFromLocalStorage</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+        </div>
+    );
 }
 
 export default App;
